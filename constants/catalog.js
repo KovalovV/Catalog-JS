a46a434ce23c39f08512bdad2a19e926593cf7b5
Vitala Kovalov <vitalya.kovalov@gmail.com>
2021-11-15 19:32:11 +0200

    Add catalog files

--- constants/catalog.js
+++ constants/catalog.js
@@ -2,7 +2,49 @@ const CATALOG = [
     {
         id: 'item1',
         name: 'iPhone 12 Mini 128Gb White (MGE43)',
-        img: '',
+        img: '../img/iphone-12-mini-white_1_.jpeg',
         price: 20199,
-    }
+    },
+    {
+        id: 'item2',
+        name: 'iPhone 13 Pro 256Gb Sierra Blue (MLVP3)',
+        img: '../img/iphone-13-pro-blue-select_1_1.png',
+        price: 41499,
+    },
+    {
+        id: 'item3',
+        name: 'iPhone 11 128GB White (MHCY3)',
+        img: '../img/iphone11-white-select1-2019_2_2.jpeg',
+        price: 19499,
+    },
+    {
+        id: 'item4',
+        name: 'iPhone 11 64GB Black (MHCP3)',
+        img: '../img/iphone11-black-select1-2019_4.jpeg',
+        price: 17399,
+    },
+    {
+        id: 'item5',
+        name: 'iPhone 12 Mini 128Gb White (MGE43)',
+        img: '../img/iphone-12-mini-white_1_.jpeg',
+        price: 20199,
+    },
+    {
+        id: 'item6',
+        name: 'iPhone Xr 128GB Black (MRY92)',
+        img: '../img/compare_iphonexr_black_large_2x.jpg',
+        price: 16199,
+    },
+    {
+        id: 'item7',
+        name: 'iPhone Xr 128GB (PRODUCT)RED Special Edition (MRYE2)',
+        img: '../img/compare_iphonexr_red_large_2x.jpg',
+        price: 15599,
+    },
+    {
+        id: 'item8',
+        name: ' iPhone Xr 128GB Coral (MRYG2)',
+        img: '../img/iphone-xr-128gb-coral_3_.jpeg',
+        price: 15199,
+    },
 ]
