6f4efd53fc6c8f7b5e1f9e7d17c0f91c76f1e62c
Vitala Kovalov <vitalya.kovalov@gmail.com>
2021-11-19 01:52:24 +0200

    Products style and render

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,15 +1,45 @@
 class Products {
 
+    constructor() {
+        this.activeButton = 'product-item__btn_active';
+        this.textAddToCart = 'Add to cart';
+        this.textRemoveFromCart = 'Remove from cart';
+    }
+
+    setItemLocationStorage(element, id) {
+        const { isPushProduct, products } = localStorageUtil.putProducts(id);
+
+        if(isPushProduct) {
+            element.classList.remove(this.activeButton);
+            element.innerText = this.textRemoveFromCart;
+        } else {
+            element.classList.add(this.activeButton);
+            element.innerText = this.textAddToCart;
+        }
+
+        headerPage.render(products.length);
+    }
+
     render() {
+        const productsStore = localStorageUtil.getProducts();
+
         let htmlCatalog = '';
 
         CATALOG.forEach(({ id, name, img, price }) => {
+            let activeBtnClass = ' ' + this.activeButton;
+            let activeBtnText = this.textAddToCart;
+
+            if(productsStore.indexOf(id) + 1) {
+                activeBtnClass = ' ';
+                activeBtnText = this.textRemoveFromCart;
+            }
+
             htmlCatalog += `
                 <li class="product-item">
                     <span class="product-item__name">${name}</span>
                     <img class="product-item__img" src="${img}" alt="Phone">
-                    <span class="product-item__price">${price}</span>
-                    <button class="product-item__btn">Add to korzina</button>
+                    <span class="product-item__price">${price.toLocaleString()} грн</span>
+                    <button class="product-item__btn${activeBtnClass}" onclick = "productPage.setItemLocationStorage(this, '${id}')">${activeBtnText}</button>
                 </li>
             `;
         })
